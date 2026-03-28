13b3ea2bcf89a2650da075785455eb496b013b1a
Bussy31 <philipp97bu@outlook.de>
2026-03-28 07:11:50 +0100

    Update

--- widgets/notiz.js
+++ widgets/notiz.js
@@ -4,7 +4,7 @@ const NotizWidget = {
         <textarea 
             v-model="widgetData.data" 
             @input="$emit('save')"
-            placeholder="Hier Text eingeben..."
+            placeholder=""
             style="background: transparent; color: white; border: none; width: 100%; min-height: 100px; resize: none; outline: none; font-family: inherit; font-size: 1rem;">
         </textarea>
     `
